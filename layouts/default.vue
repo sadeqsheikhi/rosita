facc5b1b6d840081a7f97a5a2b94918234bcbcb2
sadeqsheikhi <sadeghshaikhi77@gmail.com>
2022-01-20 17:14:17 +0330

    Base Layout Configured

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='layout-wrapper'>
-    <Nuxt/>
-    <div class='app-info'>
+    <Nuxt />
+    <div class='app-info px-2'>
       <p>
         روزیتا
       </p>
@@ -22,8 +22,28 @@ export default {
 @import "assets/scss/variables";
 .layout-wrapper {
   background: $c-tertiary;
-  background: url("../assets/img/app-bg.jpg") no-repeat;
+  background: url("assets/img/app-bg.jpg") no-repeat;
   background-position: bottom center;
   background-size: cover;
+  min-height: 100vh;
+  width: 100%;
+
+  //display
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.app-info {
+  position: absolute;
+  bottom: 1rem;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,5 @@
 <script>
 export default {
   name: 'IndexPage',
-  template: 'default',
 }
 </script>
